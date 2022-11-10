ec300f34891e70af3683b007398a6aeaa53516a5
aleksandrastaletovic99 <aleksandar.staletovic7@gmail.com>
2022-11-10 17:52:43 +0100

    dodat sadrzaj na pocetnu stranicu

--- src/components/OmiljeneSkole.jsx
+++ src/components/OmiljeneSkole.jsx
@@ -6,18 +6,23 @@ function OmiljeneSkole({skole,onAdd,onRemove }) {
  
   return (
     <div>
- 
+        {skole == null || skole=="undefined" || skole.lenght==0? 
+          <p>Niste odabrali ni jednu skolu</p>
+        : 
         <div className='sveSkole'>
               
      
-                {skole.map((t)=>(<SkolaComponenta key={t.id} skola={t}   onAdd={onAdd} onRemove={onRemove} ></SkolaComponenta>))}
-               
+              
+          {skole.map((t)=>(<SkolaComponenta key={t.id} skola={t}   onAdd={onAdd} onRemove={onRemove} ></SkolaComponenta>))}
                      
             
               
               
 
         </div>
+        
+        }
+       
     </div>
   )
 }
